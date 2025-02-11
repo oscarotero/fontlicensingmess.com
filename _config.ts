adf925e497785b3c928a790c2391ba132b94f961
Oscar Otero <oom@oscarotero.com>
2025-02-11 19:56:20 +0100

    tweaks

--- _config.ts
+++ _config.ts
@@ -1,9 +1,16 @@
 import lume from "lume/mod.ts";
 import lightningcss from "lume/plugins/lightningcss.ts";
+import favicon from "lume/plugins/favicon.ts";
+import metas from "lume/plugins/metas.ts";
 
 const site = lume();
 
 site.use(lightningcss());
+site.use(metas());
+site.use(favicon({
+  input: "assets/logo.svg",
+}));
+
 site.copy("assets");
 
 export default site;
