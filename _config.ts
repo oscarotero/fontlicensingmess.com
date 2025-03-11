42a32c7e912266d0466779f6def9f2ef4c9d5e50
Oscar Otero <oom@oscarotero.com>
2025-03-11 20:33:34 +0100

    avances

--- _config.ts
+++ _config.ts
@@ -3,6 +3,7 @@ import lightningcss from "lume/plugins/lightningcss.ts";
 import favicon from "lume/plugins/favicon.ts";
 import metas from "lume/plugins/metas.ts";
 import inline from "lume/plugins/inline.ts";
+import multilanguage from "lume/plugins/multilanguage.ts";
 
 const site = lume();
 
@@ -11,6 +12,10 @@ site.use(metas());
 site.use(inline());
 site.use(favicon({
   input: "assets/logo.svg",
+}));
+site.use(multilanguage({
+  defaultLanguage: "en",
+  languages: ["en", "es"],
 }));
 
 site.copy("assets");
