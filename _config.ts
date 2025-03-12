7bc5302aaf52317386f28fdf6bbc987fa5d8c863
Oscar Otero <oom@oscarotero.com>
2025-03-12 23:45:08 +0100

    navegación

--- _config.ts
+++ _config.ts
@@ -4,12 +4,14 @@ import favicon from "lume/plugins/favicon.ts";
 import metas from "lume/plugins/metas.ts";
 import inline from "lume/plugins/inline.ts";
 import multilanguage from "lume/plugins/multilanguage.ts";
+import nav from "lume/plugins/nav.ts";
 
 const site = lume();
 
 site.use(lightningcss());
 site.use(metas());
 site.use(inline());
+site.use(nav());
 site.use(favicon({
   input: "assets/logo.svg",
 }));
