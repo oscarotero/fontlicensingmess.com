26d61112fc1ea07b6ba16fcc142c4bfb0af227d1
Oscar Otero <oom@oscarotero.com>
2025-03-25 18:09:16 +0100

    demographic data

--- _config.ts
+++ _config.ts
@@ -26,4 +26,37 @@ site.use(multilanguage({
 
 site.copy("assets");
 
+interface FoundryCSV {
+  Nombre: string;
+  "URL Licensing": string;
+  "Base Country": string;
+  "Run by women": "No" | "Yes";
+}
+interface Foundry {
+  name: string;
+  url?: string;
+  country: string;
+  women: boolean;
+}
+
+site.filter("demographics", (content: FoundryCSV[]) => {
+  const foundries: Foundry[] = [];
+
+  for (const foundry of content) {
+    const countries = foundry["Base Country"].split(",").map((c) => c.trim());
+    for (const country of countries) {
+      foundries.push({
+        name: foundry.Nombre,
+        url: foundry["URL Licensing"] || undefined,
+        country,
+        women: foundry["Run by women"] === "Yes",
+      });
+    }
+  }
+
+  foundries.sort((a, b) => a.country.localeCompare(b.country));
+
+  return Object.groupBy(foundries, (f) => f.country);
+});
+
 export default site;
